738aeb4fddc8d268a8e89771424732b9fc518ef0
NazarKatasonov <nazariikryt22@gmail.com>
2025-06-24 13:38:51 +0000

    ihygbiy

---
+++ index1.js
@@ -0,0 +1,13 @@
+const age = 14
+const name = "Nazarii"
+let isStudent = false
+console.log(isStudent)
+let myString = "не май 100 ру**ів а май 100 друзів кожен дасть по 2 ру**я буде 200 у тебе"
+console.log(myString)
+let myNumber = 52
+let mySecondNumber = 10
+console.log(myNumber+mySecondNumber)
+let myNull = null
+console.log(myNull)
+const UserName = prompt("Введіть своє ім’я")
+console.log("Привіт тебе звати", UserName )
