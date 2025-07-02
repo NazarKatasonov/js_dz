cf237de84bc61d469a420cd60541951d0a882931
NazarKatasonov <nazariikryt22@gmail.com>
2025-07-02 10:49:20 +0000

    ugirjure

--- index1.js
+++ index1.js
@@ -1,13 +1,53 @@
-const age = 14
-const name = "Nazarii"
-let isStudent = false
-console.log(isStudent)
-let myString = "не май 100 ру**ів а май 100 друзів кожен дасть по 2 ру**я буде 200 у тебе"
-console.log(myString)
-let myNumber = 52
-let mySecondNumber = 10
-console.log(myNumber+mySecondNumber)
-let myNull = null
-console.log(myNull)
-const UserName = prompt("Введіть своє ім’я")
-console.log("Привіт тебе звати", UserName )
+//const age = 14
+//const name = "Nazarii"
+//let isStudent = false
+//console.log(isStudent)
+//let myString = "не май 100 ру**ів а май 100 друзів кожен дасть по 2 ру**я буде 200 у тебе"
+//console.log(myString)
+//let myNumber = 52
+//let mySecondNumber = 10
+//console.log(myNumber+mySecondNumber)
+//let myNull = null
+//console.log(myNull)
+//const UserName = prompt("Введіть своє ім’я")
+//console.log("Привіт тебе звати", UserName )
+
+
+let celsius = 36
+let fahrenheit = (celsius * 9/5) + 32
+console.log(fahrenheit)
+//Створити змінну totalPrice для зберігання суми покупки в магазині. Застосувати знижку discount в розмірі 10% до 
+// цієї суми за допомогою оператора множення. Результат зберегти в змінній discountedPrice та вивести результат в консоль.
+let totalPrice = 5000;
+let discount = 0.1
+totalPrice = (totalPrice - (totalPrice * discount))
+console.log(totalPrice)
+//Це були завдання 4 та 1 далі по черзі
+
+let daysInMonth = 31
+let hoursInMonth = (daysInMonth * 24)
+console.log(hoursInMonth)
+let minutesInMonth = (daysInMonth * 1440)
+console.log(minutesInMonth)
+
+let health = 100
+let energy = 50
+console.log(health/2)
+console.log(energy/1.5)
+
+let floatNumber = 12.78
+console.log(Math.floor(floatNumber))
+let = floatString = "45.67"
+console.log(parseFloat(floatString))
+
+const intString = "123"
+console.log(parseInt(intString))
+
+const number = 67
+console.log(Math.sqrt(number))
+
+const integer = 42
+const stringNumber = "256"
+console.log(parseInt(stringNumber))
+let convertedInt = integer + stringNumber
+console.log(toString(convertedInt))
