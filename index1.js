bbfec86992f549418c9bf32c638a332977f0f6f5
NazarKatasonov <nazariikryt22@gmail.com>
2025-08-24 18:51:43 +0000

    hgerggearghewgeFE

--- index1.js
+++ index1.js
@@ -384,3 +384,18 @@
 //   'Картка-5',
 // ];
 
+
+
+// // Картку яку треба оновити
+// const cardToUpdate = 'Картка-4';
+// // Нове значення
+// const newCard = 'Оновлена картка-4';
+
+// // Знаходимо індекс
+// const index = cards.indexOf(cardToUpdate);
+
+// if (index !== -1) {
+//   cards[index] = newCard;
+// }
+
+// console.log(cards);
