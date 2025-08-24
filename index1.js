d6602b2efd7ecfebcac3386c28386661880d54f1
NazarKatasonov <nazariikryt22@gmail.com>
2025-08-24 15:46:17 +0000

    Update index1.js

--- index1.js
+++ index1.js
@@ -84,84 +84,87 @@
 // Перевірте, чи обидві змінні не є порожніми. Якщо обидва поля заповнені,
 //  вивести повідомлення "Обидва поля заповнені", якщо ні — "Не всі поля заповнені".
 
-let text1 = "немаПробілу"
-let text2 = " з пробілом "
+// let text1 = "немаПробілу"
+// let text2 = " з пробілом "
 
-if (text1.includes("")){
-    console.log("Немає пробілу))")
-}else if (text1.includes( )) {
-    console.log("Є пробіл")
-}
-
-if (text2.includes(" ")){
-    console.log("Є пробіл")
-} else if (text2.includes()) {
-    console.log("Немає пробілу))")
-}
-
-
-
-//Створіть дві змінні для зберігання числових значень.
-//  Обчисліть їх суму. Якщо сума більше 10 — виведіть "Сума більша за 10", якщо ні — "Сума менша або дорівнює 10".
-
-
-let number1 = 12
-let number2 = 8
-if(number1<10){
-    console.log("сума менша за 10")
-}else if(number1>10){
-    console.log("сума більша 10")
-}
-
-if(number2<10){
-    console.log("сума менша за 10")
-}else if(number2>10){
-    console.log("сума більша 10")
-}
-
-//Створіть змінну для зберігання тексту. Перевірте, чи містить цей текст слово "JavaScript".
-//  Якщо містить, виведіть "Текст містить слово JavaScript", якщо ні — "Текст не містить слово JavaScript".
-const text3 = "tyalet"
-if (text3.includes("JavaScript")){
-    console.log("Текст містить слово Javascript")
-} else if(text3.includes("tyalet")){
-    console.log("Текст не містить Javscript a містить слово tyalet")
-}
-
-//Створіть змінну для зберігання числа. Перевірте, чи це число більше 10 і менше 20.
-//Якщо так — виведіть "Число входить в діапазон від 10 до 20", якщо ні — "Число не входить в діапазон від 10 до 20".
-const newNumber = 13
-if(newNumber>10 && newNumber<20){
-    console.log("Число входить в діапазон від 10 до 20")
-}
-//Число входить в діапазон від 10 до 20
-
-//Створіть змінні для зберігання значень полів (ім'я, email, пароль). Перевірте, чи ім'я містить не менше 3 символів,
-//  чи email містить символ @ та крапку після нього, а пароль не менше 6 символів.
-//  Якщо всі умови виконані — виведіть "Перенаправлення на іншу сторінку", якщо ні — "Помилка: неправильне заповнення".
-const name = "Nazarii"
-const gmail = "skibidityaletov@gmail.com"
-const password  = "162568655"
-// if (name.length > 3){ 
-// console.log("правильно")
-// }else if (name.length < 3){
-//     console.log("спробуйте ще раз")
+// if (text1.includes("")){
+//     console.log("Немає пробілу))")
+// }else if (text1.includes( )) {
+//     console.log("Є пробіл")
 // }
 
-// if (gmail.includes("@" || ".")){
-//     console.log("ви ввели правильну пошту")
-// }
-// else if(gmail.includes(! "@" || ".")){
-//     console.log("не правильна пошта")
+// if (text2.includes(" ")){
+//     console.log("Є пробіл")
+// } else if (text2.includes()) {
+//     console.log("Немає пробілу))")
 // }
 
-// if (password.length<6){
-//     console.log("мінімум 6 цифр у паролі!!!")
-// }else if (password.length>=6){
-//     console.log("все чьотінько");
+
+
+// //Створіть дві змінні для зберігання числових значень.
+// //  Обчисліть їх суму. Якщо сума більше 10 — виведіть "Сума більша за 10", якщо ні — "Сума менша або дорівнює 10".
+
+
+// let number1 = 12
+// let number2 = 8
+// if(number1<10){
+//     console.log("сума менша за 10")
+// }else if(number1>10){
+//     console.log("сума більша 10")
 // }
-if(name.length>3 && gmail.includes("@"||".") && password.length>=6){
-    console.log("всьо чотка братік")
-} else if (name.length<3 && gmail.includes(!"@"||".") && password.length>6){
-    console.log("Браточік ну шо за діла")
-}
+
+// if(number2<10){
+//     console.log("сума менша за 10")
+// }else if(number2>10){
+//     console.log("сума більша 10")
+// }
+
+// //Створіть змінну для зберігання тексту. Перевірте, чи містить цей текст слово "JavaScript".
+// //  Якщо містить, виведіть "Текст містить слово JavaScript", якщо ні — "Текст не містить слово JavaScript".
+// const text3 = "tyalet"
+// if (text3.includes("JavaScript")){
+//     console.log("Текст містить слово Javascript")
+// } else if(text3.includes("tyalet")){
+//     console.log("Текст не містить Javscript a містить слово tyalet")
+// }
+
+// //Створіть змінну для зберігання числа. Перевірте, чи це число більше 10 і менше 20.
+// //Якщо так — виведіть "Число входить в діапазон від 10 до 20", якщо ні — "Число не входить в діапазон від 10 до 20".
+// const newNumber = 13
+// if(newNumber>10 && newNumber<20){
+//     console.log("Число входить в діапазон від 10 до 20")
+// }
+// //Число входить в діапазон від 10 до 20
+
+// //Створіть змінні для зберігання значень полів (ім'я, email, пароль). Перевірте, чи ім'я містить не менше 3 символів,
+// //  чи email містить символ @ та крапку після нього, а пароль не менше 6 символів.
+// //  Якщо всі умови виконані — виведіть "Перенаправлення на іншу сторінку", якщо ні — "Помилка: неправильне заповнення".
+// const name = "Nazarii"
+// const gmail = "skibidityaletov@gmail.com"
+// const password  = "162568655"
+// // if (name.length > 3){ 
+// // console.log("правильно")
+// // }else if (name.length < 3){
+// //     console.log("спробуйте ще раз")
+// // }
+
+// // if (gmail.includes("@" || ".")){
+// //     console.log("ви ввели правильну пошту")
+// // }
+// // else if(gmail.includes(! "@" || ".")){
+// //     console.log("не правильна пошта")
+// // }
+
+// // if (password.length<6){
+// //     console.log("мінімум 6 цифр у паролі!!!")
+// // }else if (password.length>=6){
+// //     console.log("все чьотінько");
+// // }
+// if(name.length>3 && gmail.includes("@"||".") && password.length>=6){
+//     console.log("всьо чотка братік")
+// } else if (name.length<3 && gmail.includes(!"@"||".") && password.length>6){
+//     console.log("Браточік ну шо за діла")
+// }
+
+
+//Створіть змінну, що симулює вибір варіанта зі списку. Виводьте повідомлення залежно від обраного варіанта: "Кава", "Чай" або "Сік". Використайте switch
